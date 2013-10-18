dbc0eaabbbb5e5ba9fc6eee9a541d188e03972ef
Harrisam <samantha.s.harris@googlemail.com>
2013-10-18 19:10:29 +0100

    31 down ...

--- questions.rb
+++ questions.rb
@@ -193,7 +193,7 @@ end
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
-	
+	email.scan(/@(.*?)\.com/m).flatten.pop
 end
 
 # capitalize the first letter in each word of a string, 
